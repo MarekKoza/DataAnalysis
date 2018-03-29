9fe6d3b3f52a2cd1a3b14eaa812c77f2c7fc0f86
Marek Koza <kozamarek@gmail.com>
2018-03-29 22:27:04 +0200

    Changes in class Test1

--- Test/src/TestingGitHub/Test1.java
+++ Test/src/TestingGitHub/Test1.java
@@ -1,8 +1,15 @@
 package TestingGitHub;
 
 public class Test1 {
-	public Test1() {
-		System.out.println("Testoweanie po³¹czenia z GitHubem");
+	
+		public Test1() {
+			System.out.println("Testoweanie po³¹czenia z GitHubem");
+		}
+
+public static void main (String[] args) {
+	
+	Test1 testing = new Test1();
+	
 	}
 
 }
